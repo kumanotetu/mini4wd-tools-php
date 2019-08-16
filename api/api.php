adc0a25069bd218ac21e5ef23e286cec0d803ee9
kumanotetu <kumanotetu@gmail.com>
2019-08-16 17:11:47 +0900

    JSONパラメータ名を修正

--- api/api.php
+++ api/api.php
@@ -39,13 +39,13 @@ if(empty($message)) {
 
     $status = ['status' => 'OK'];
     $j_param = [
-        'result-parameter' => [
-            'f-height-out' => round($f_height_out * 100) / 100,
-            'f-height-in'  => round($f_height_in  * 100) / 100,
-            'r-height-out' => round($r_height_out * 100) / 100,
-            'r-height-in'  => round($r_height_in  * 100) / 100,
-            'f-angle' => round($f_angle * 10) / 10,
-            'r-angle' => round($r_angle * 10) / 10
+        'result_parameter' => [
+            'f_height_out' => round($f_height_out * 100) / 100,
+            'f_height_in'  => round($f_height_in  * 100) / 100,
+            'r_height_out' => round($r_height_out * 100) / 100,
+            'r_height_in'  => round($r_height_in  * 100) / 100,
+            'f_angle' => round($f_angle * 10) / 10,
+            'r_angle' => round($r_angle * 10) / 10
         ]
     ];
     $j_value = viewParams($wh_base, $t_size, $f_length, $f_width, $r_length, $r_width, $check_bank);
@@ -112,13 +112,13 @@ function checkMachineParameter() {
 function viewParams($wh_base, $t_size, $f_length, $f_width, $r_length, $r_width, $check_bank) {
     $result = [
         'input-parameter' => [
-            'wh-base'  => $wh_base,
-            't-size'   => $t_size,
-            'f-length' => $f_length,
-            'f-width'  => $f_width,
-            'r-length' => $r_length,
-            'r-width'  => $r_width,
-            'check-bank' => $check_bank
+            'wh_base'  => $wh_base,
+            't_size'   => $t_size,
+            'f_length' => $f_length,
+            'f_width'  => $f_width,
+            'r_length' => $r_length,
+            'r_width'  => $r_width,
+            'check_bank' => $check_bank
         ]
     ];
     return  $result;
@@ -129,29 +129,29 @@ function headDescription() {
         'title' => 'ミニ四駆ブレーキ高セッティングAPI',
         'description' => [
             'comment' => 'マシンパラメータと調べたい傾斜番号を渡すことでギリギリ接触する前後のブレーキ高を計算するAPI',
-            'input-parameter' => [
-                'wh-base'  => 'ホイールベース(mm)',
-                't-size'   => 'タイヤ直径(mm)',
-                'f-length' => '前車軸からフロントブレーキ内側までの距離(mm)',
-                'f-width'  => 'フロントブレーキ内側から外側までの距離、ブレーキの幅(mm)',
-                'r-length' => '後車軸からリアブレーキ内側までの距離(mm)',
-                'r-width'  => 'リアブレーキ内側から外側までの距離、ブレーキの幅(mm)',
-                'check-bank' => [
+            'input_parameter' => [
+                'wh_base'  => 'ホイールベース(mm)',
+                't_size'   => 'タイヤ直径(mm)',
+                'f_length' => '前車軸からフロントブレーキ内側までの距離(mm)',
+                'f_width'  => 'フロントブレーキ内側から外側までの距離、ブレーキの幅(mm)',
+                'r_length' => '後車軸からリアブレーキ内側までの距離(mm)',
+                'r_width'  => 'リアブレーキ内側から外側までの距離、ブレーキの幅(mm)',
+                'check_bank' => [
                     '1' => 'JCJCバンク',
                     '6' => 'ライジングファントムチェンジャー'
                 ]
             ],
-            'result-parameter' => [
-                'f-height-out' => 'フロントブレーキ外側の高さ(mm)',
-                'f-height-in'  => 'フロントブレーキ内側の高さ(mm)',
-                'r-height-out' => 'リアブレーキ外側の高さ(mm)',
-                'r-height-in'  => 'リアブレーキ内側の高さ(mm)',
-                'f-angle' => 'フロントブレーキの角度(°)',
-                'r-angle' => 'リアブレーキの角度(°)',
+            'result_parameter' => [
+                'f_height_out' => 'フロントブレーキ外側の高さ(mm)',
+                'f_height_in'  => 'フロントブレーキ内側の高さ(mm)',
+                'r_height_out' => 'リアブレーキ外側の高さ(mm)',
+                'r_height_in'  => 'リアブレーキ内側の高さ(mm)',
+                'f_angle' => 'フロントブレーキの角度(°)',
+                'r_angle' => 'リアブレーキの角度(°)',
             ]
         ],
         'usage' =>  (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . '/api/api.php?wh-base=80&t-size=24&f-length=20&f-width=6&r-length=30&r-width=6&check-bank=1',
-        'api-version' => '1.0.0'
+        'api_version' => '1.0.0'
     ];
 }
 ?>
